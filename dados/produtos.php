9bae06d85ad3a002cbfe6e416230d41af22f1707
W4NDR <89627099+W4NDR@users.noreply.github.com>
2021-08-27 19:52:08 -0300

    fotos

--- dados/produtos.php
+++ dados/produtos.php
@@ -1,27 +1,24 @@
 <?php
 
-$produtos['trufa'] = array(
-    'nome' => 'Trufa de Chocolate',
-    'descricao' => 'Trufa de chocolate crocante recheada com creme de avelã com toque de caramelo salgado e cardamomo.
+$produtos['Decoracoes'] = array(
+    'nome' => 'Decoracoes de Festas',
+    'descricao' => 'Festa completa ou apenas parcial.
     <br/><br/>
     Ideal para ocasiões em que o requinte é fundamental.
     <br/><br/>
-    Pedido mínimo de 10 unidades.',
-    'preco' => 6.7,
-    'imagem' => 'bombom.jpg'
+    Reservas disponiveis na agenda.',
+    'imagem' => 'decor1.jpg'
 );
 
-$produtos['cupcake'] = array(
-    'nome' => 'Cupcake Delícia',
-    'descricao' => 'Deliciosos cupcakes de diversos sabores para todas as ocasiões',
-    'preco' => 4.5,
-    'imagem' => 'cupcake_2.jpg'
+$produtos['Painel'] = array(
+    'nome' => 'Painel',
+    'descricao' => 'Festas',
+    'imagem' => 'decor2.jpg'
 );
 
-$produtos['torta_chocolate'] = array(
-    'nome' => 'Torta de Chocolate',
-    'descricao' => 'Uma torta de chocolate como você nunca experimentou. Cuidadosamente preparada com ingredientes selecionados.
-    <br/>Serve 6 pessoas.',
-    'preco' => 6.7,
-    'imagem' => 'torta_chocolate_1.jpg'
+$produtos['Casamentos'] = array(
+    'nome' => 'Casamento',
+    'descricao' => 'Festa completa para voce e sua familia.
+    <br/>voce escolhe a quantidade de pessoas.',
+    'imagem' => 'decor3.jpg'
 );
