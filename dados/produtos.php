284343354c5df0e64efc50a8f7acbcc3ea0542d0
W4NDR <89627099+W4NDR@users.noreply.github.com>
2021-08-27 17:34:25 -0300

    products

---
+++ dados/produtos.php
@@ -0,0 +1,27 @@
+<?php
+
+$produtos['trufa'] = array(
+    'nome' => 'Trufa de Chocolate',
+    'descricao' => 'Trufa de chocolate crocante recheada com creme de avelã com toque de caramelo salgado e cardamomo.
+    <br/><br/>
+    Ideal para ocasiões em que o requinte é fundamental.
+    <br/><br/>
+    Pedido mínimo de 10 unidades.',
+    'preco' => 6.7,
+    'imagem' => 'bombom.jpg'
+);
+
+$produtos['cupcake'] = array(
+    'nome' => 'Cupcake Delícia',
+    'descricao' => 'Deliciosos cupcakes de diversos sabores para todas as ocasiões',
+    'preco' => 4.5,
+    'imagem' => 'cupcake_2.jpg'
+);
+
+$produtos['torta_chocolate'] = array(
+    'nome' => 'Torta de Chocolate',
+    'descricao' => 'Uma torta de chocolate como você nunca experimentou. Cuidadosamente preparada com ingredientes selecionados.
+    <br/>Serve 6 pessoas.',
+    'preco' => 6.7,
+    'imagem' => 'torta_chocolate_1.jpg'
+);
