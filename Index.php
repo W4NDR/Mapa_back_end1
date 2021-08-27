e05c1212a30a8d2842a1bf6727f712bd346e214a
W4NDR <89627099+W4NDR@users.noreply.github.com>
2021-08-27 20:59:00 -0300

    atualizacao

--- Index.php
+++ Index.php
@@ -4,7 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mundo das Festas</title>
+    <title>Caramelo - Doces Artesanais</title>
+    <link rel="stylesheet" href="css/page.css">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/contato.css">
+    <link rel="stylesheet" href="css/home.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bitter:wght@300;400;700&display=swap" rel="stylesheet">
 </head>
 <body>
     <header>
