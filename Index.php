a4a1d565ae69aae30c94f94ff9427b8bed624ee6
W4NDR <89627099+W4NDR@users.noreply.github.com>
2021-08-27 21:11:49 -0300

    index

--- Index.php
+++ Index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Caramelo - Doces Artesanais</title>
+    <title>Mundo das Festas</title>
     <link rel="stylesheet" href="css/page.css">
     <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="css/contato.css">
